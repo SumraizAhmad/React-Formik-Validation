67718d69360eecc19b00c23fbf2e12639e7f53d8
SumraizAhmad <sumraizrandhawa@gmail.com>
2023-02-12 10:13:34 +0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Formik from './Components/Formik/Formik';
+
+function App() {
+  return (
+    <div className="App">
+      <Formik />
+    </div>
+  );
+}
+
+export default App;
